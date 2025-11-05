2ce3da2dd41365c4f105f514a0b6bbef2e0b0467
dealeronGirmada <paramveer.singh@dealeron.com>
2025-11-05 13:27:52 +0530

    corrected update url

--- main/script.user.js
+++ main/script.user.js
@@ -5,8 +5,8 @@
 // @version      1.0.0
 // @author       Pratik Chabria
 // @match        *://*/*
-// @updateURL    https://raw.githubcontent.com/DealerOnGirmada/office-tampermonkey-scripts/main/script.user.js
-// @downloadURL  https://raw.githubcontent.com/DealerOnGirmada/office-tampermonkey-scripts/main/script.user.js
+// @updateURL    https://raw.githubusercontent.com/DealerOnGirmada/office-tampermonkey-scripts/main/main/script.user.js
+// @downloadURL  https://raw.githubusercontent.com/DealerOnGirmada/office-tampermonkey-scripts/main/main/script.user.js
 // @grant        GM_getValue
 // @grant        GM_setValue
 // @grant        GM_notification
